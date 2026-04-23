ffae8ab90ffef2f560d2d6fb2553e07de941df5c
Supy <sofia.v.amarilla@gmail.com>
2026-04-23 14:53:39 -0300

    feat/added create movies

---
+++ src/hooks/local/useCreateLocalMovie.ts
@@ -0,0 +1,16 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import type { LocalMovie } from "../../types/localMovie";
+import { Serverservice } from "../../service/server/serverService";
+
+export const useCreateLocalMovie = () => {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: (localMovie: Partial<LocalMovie>) => Serverservice.createMovie(localMovie),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["localMovies"]})
+        }
+    })
+
+    return mutation;
+}
